4fa7d9ac4b26e3c347a023568e576290246aaf9e
John Tran <jptran318@gmail.com>
2017-06-26 23:09:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+[![Build Status](https://img.shields.io/travis/jooohhn/strongr.svg?style=flat-square)](https://travis-ci.com/jooohhn/strongr)
+
+
+## Useful Commands
+- `git push --no-verify`: Pushes without Husky complaining
---
+++ src/server/render-app.js
@@ -0,0 +1,24 @@
+// @flow
+import Helmet from 'react-helmet';
+import { APP_CONTAINER_CLASS, STATIC_PATH, WDS_PORT } from '../shared/config';
+import { isProd } from '../shared/util';
+
+const renderApp = () => {
+  const head = Helmet.rewind();
+  return `<!doctype html>
+<html>
+  <head>
+     ${head.title}
+     ${head.meta}
+    <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
+  </head>
+  <body>
+    <div class="${APP_CONTAINER_CLASS}"></div>
+    <script src="${isProd
+      ? STATIC_PATH
+      : `http://localhost:${WDS_PORT}/dist`}/js/bundle.js"></script>
+  </body>
+</html>`;
+};
+
+export default renderApp;
