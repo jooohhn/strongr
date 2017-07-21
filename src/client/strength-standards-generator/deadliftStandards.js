888a1178506a66ebc54abfa89328d229cd3340e8
John Tran <jptran318@gmail.com>
2017-07-21 15:49:32 -0700

    Moved order of props/state vars. Clarified bodyweight and exercise weight. Added intial progress bar

--- src/client/strength-standards-generator/deadliftStandards.js
+++ src/client/strength-standards-generator/deadliftStandards.js
@@ -2,38 +2,38 @@
 import type { StrengthStandardsType } from '../types';
 
 export default function getDeadliftStandards(
-  weight: number
+  bodyweight: number
 ): StrengthStandardsType {
   return {
     untrained: Math.round(
       -90.39365 +
-        2.311359 * weight -
-        0.006718996 * weight ** 2 +
-        0.000006670664 * weight ** 3
+        2.311359 * bodyweight -
+        0.006718996 * bodyweight ** 2 +
+        0.000006670664 * bodyweight ** 3
     ),
     novice: Math.round(
       -151.331 +
-        4.045437 * weight -
-        0.01126525 * weight ** 2 +
-        0.00001051854 * weight ** 3
+        4.045437 * bodyweight -
+        0.01126525 * bodyweight ** 2 +
+        0.00001051854 * bodyweight ** 3
     ),
     intermediate: Math.round(
       -192.2271 +
-        4.850264 * weight -
-        0.01355166 * weight ** 2 +
-        0.0000124415 * weight ** 3
+        4.850264 * bodyweight -
+        0.01355166 * bodyweight ** 2 +
+        0.0000124415 * bodyweight ** 3
     ),
     advanced: Math.round(
       -240.1869 +
-        6.75806 * weight -
-        0.02033099 * weight ** 2 +
-        0.00002025135 * weight ** 3
+        6.75806 * bodyweight -
+        0.02033099 * bodyweight ** 2 +
+        0.00002025135 * bodyweight ** 3
     ),
     elite: Math.round(
       -370.9264 +
-        10.13514 * weight -
-        0.03579004 * weight ** 2 +
-        0.00004294107 * weight ** 3
+        10.13514 * bodyweight -
+        0.03579004 * bodyweight ** 2 +
+        0.00004294107 * bodyweight ** 3
     )
   };
 }
